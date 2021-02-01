9ef07c3f0b89c49532c33a985da573a8a915fb1a
Lila NICKLER <lila.nickler@etu.imt-lille-douai.fr>
2021-02-01 16:39:37 +0100

    Barre de navigation: 3 pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,38 +34,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Mes Séries et Films',
+      style: optionStyle,
+    ),
+    Text(
+      'Mes Jeux Vidéo',
+      style: optionStyle,
+    ),
+  ];
 
-  void _incrementCounter() {
+  void _onItemTapped(int index) {
     setState(() {
-      _counter++;
+      _selectedIndex = index;
     });
   }
-
   @override
   Widget build(BuildContext context) {
+    final series=[MediaItem(name: "Les Simpsons",description: "Les simpsons", image: "https://fr.wikipedia.org/wiki/Les_Simpson#/media/Fichier:Les_simpson_logo_France.png")];
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Bienvenu dans votre médiathèque',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.ondemand_video_rounded ),
+            label: 'Séries & Films',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.videogame_asset_outlined),
+            label: 'Jeux Vidéo',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class MediaItem {
+  String name;
+  String description;
+  String image;
+
+  MediaItem({this.name, this.description,this.image});
+  String getName ()
+  {
+   return this.name ;
+  }
+}
+
